eb744fa30d9549eebc56725326bdcd39823fb3a9
Angelo G. Del Regno <kholk11@gmail.com>
2015-04-26 22:17:17 -0300

    [PATCH] fs: aio: Optimization for SSD-only machines
    If the system is running on NAND-based storage, without
    mechanical drives, we don't need to manage the plug/unplug
    of the actual block because we don't have to count the
    time for the head to reach that disk physical location.
    Solid state storage have no head to move and no location
    to "physically reach", as the block is being retrieved
    in the exact moment we request it.
    
    The do_io_submit function expects the storage device to
    be ready to deliver the block (aka - next block of the
    queue to process), therefore it's safe to remove useless
    waits on systems using only that type of storage.
    
    This patch adds a configuration option to enable the
    SSD-only optimization for the Linux Asynchronous IO
    framework.
    Enabling it on systems with rotational storage (or mixed)
    WILL hit storage performance A LOT.
    
    Change-Id: If059ae34aed8200dffcf73a0a6696e2b926e9682
    
    Enable AIO_SSD_ONLY

--- fs/aio.c
+++ fs/aio.c
@@ -1684,8 +1684,11 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 	struct kiocb_batch batch;
+
+#ifndef CONFIG_AIO_SSD_ONLY
+	struct blk_plug plug;
+#endif
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1704,7 +1707,9 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 
 	kiocb_batch_init(&batch, nr);
 
+#ifndef CONFIG_AIO_SSD_ONLY
 	blk_start_plug(&plug);
+#endif
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1728,7 +1733,10 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
+
+#ifndef CONFIG_AIO_SSD_ONLY
 	blk_finish_plug(&plug);
+#endif
 
 	kiocb_batch_free(ctx, &batch);
 	put_ioctx(ctx);
